977546a3c3531b1bfff86db435270b3638fc575f
Temani <temani@keysysconsulting.com>
2019-05-24 15:41:58 -0500

    Refactored code

--- Assets/Scripts/NumberWizard.cs
+++ Assets/Scripts/NumberWizard.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class NumberWizard : MonoBehaviour
 {
-    int maxNumber = 1000;
-    int minNumber = 1;
-    int initialGuess = 500;
+    int maxNumber, minNumber, initialGuess;
 
     // Start is called before the first frame update
     void Start()
     {
+        StartGame();
+    }   
+
+    private void StartGame()
+    {
+        maxNumber = 1000;
+        minNumber = 1;
+        initialGuess = 500;
         Debug.Log("Welcome to Number Wizard!");
         Debug.Log("Pick a number between 1-1000, but don't tell me what it is...");
         Debug.Log("The highest number you can pick is " + maxNumber + ".");
@@ -23,16 +29,26 @@ public class NumberWizard : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow)){
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
             minNumber = initialGuess;
-            initialGuess = (maxNumber + minNumber) / 2;
-            Debug.Log("Is it higher or lower than " + initialGuess);
-        }else if (Input.GetKeyDown(KeyCode.DownArrow)){
-            maxNumber = initialGuess;
-            initialGuess = (maxNumber + minNumber) / 2;
-            Debug.Log("Is it higher or lower than " + initialGuess);
-        }else if (Input.GetKeyDown(KeyCode.Return)){
-            Debug.Log("I guessed correctly");
+            NextGuess();
         }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            maxNumber = initialGuess;
+            NextGuess(); ;
+        }
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("I guessed correctly");
+            StartGame();
+        }
+    }
+
+    private void NextGuess()
+    {
+        initialGuess = (maxNumber + minNumber) / 2;
+        Debug.Log("Is it higher or lower than " + initialGuess);
     }
 }
